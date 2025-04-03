d91640273ce79b664022d219f61b094fa7eff3cb
ganeshadvait <ganesh@advaitlabs.com>
2025-04-04 01:19:10 +0530

    headless

--- server.js
+++ server.js
@@ -18,7 +18,9 @@ app.get('/fetch-video', async (req, res) => {
     if (!url) return res.status(400).json({ error: 'No URL provided' });
 
     try {
-        const browser = await puppeteer.launch({ headless: true });
+        const browser = await puppeteer.launch({ headless: true,
+            args: ["--no-sandbox", "--disable-setuid-sandbox"],
+         });
         const page = await browser.newPage();
         await page.setUserAgent(
             'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
